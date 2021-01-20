8593cbce43c9f4c8fa68d841be9553b624614b88
Tariq Islam <md-tariqul.islam@mnsu.edu>
2021-01-19 23:17:54 -0600

    Added static root

--- todowoo/settings.py
+++ todowoo/settings.py
@@ -1,5 +1,6 @@
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,3 +109,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
